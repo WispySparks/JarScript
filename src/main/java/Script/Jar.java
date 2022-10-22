233e0e0e54796957a693152554f5ecaa36b31dd9
WispySparks <wispysparks@gmail.com>
2022-10-22 00:19:29 -0500

    manifest, fix command

--- src/main/java/Script/Jar.java
+++ src/main/java/Script/Jar.java
@@ -1,19 +1,24 @@
 package main.java.Script;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Jar {
 
-    Jar(String jarPath, String jarName) {
+    String mainClass;
+
+    Jar(String jarPath, String jarName, String mainClass) {
+        this.mainClass = mainClass;
         createJar(jarPath, jarName);
     }
 
     private void createJar(String path, String name) {
+        File manifest = createManifest(path);
         File jarFile = new File(path + "\\" + name + ".jar"); // make a jar file reference to test if it already exists
         if (!jarFile.exists()) {
-            ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c jar cvf " +  
-            "\"" + jarFile.getAbsolutePath() + "\"" + " -C bin");
+            ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c jar -c -v -f " +  
+            "\"" + jarFile.getAbsolutePath() + "\" -m \"" + manifest.getAbsolutePath() + "\" -C bin .");
             builder.inheritIO();
             try {
                 builder.start();    // create the jar file
@@ -21,6 +26,19 @@ public class Jar {
                 e.printStackTrace();
             }
         } 
+    }
+
+    private File createManifest(String path) {
+        File file = new File("");
+        try {
+            file = File.createTempFile("mf-", ".txt");
+            FileWriter writer = new FileWriter(file); // write everything to it
+            writer.write("Manifest-Version: 1.0" + "\r\n" + "Main-Class: " + mainClass + "\r\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
     }
 
 }
--- src/main/java/Script/Main.java
+++ src/main/java/Script/Main.java
@@ -1,16 +1,22 @@
 package main.java.Script;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) { 
+        Scanner s = new Scanner(System.in);
         Organizer organizer = new Organizer(); // create folder that everything goes inside
-        String path = organizer.getPath(); // get the path of the folder
-        String name = organizer.getName(); // get name of project
-        new Jar(path, name); // create jar file
+        String folderPath = organizer.getPath(); // get the path of the folder
+        String folderName = organizer.getName(); // get name of project
+        System.out.println("Input main class path. e.g. com.example.package.Main");
+        String mainClass = s.nextLine();
+        new Jar(folderPath, folderName, mainClass); // create jar file
+        s.close();
         // sleep();
-        // new Runtime(name + ".jar", name, path); // create runtime for jar file //Todo runtime still isnt created sometimes
+        // new Runtime(folderName + ".jar", folderName, folderPath); // create runtime for jar file //Todo runtime still isnt created sometimes
         // sleep();
-        // new Batch(name, path); // create batch file to run jar file with runtime
+        // new Batch(folderName, folderPath); // create batch file to run jar file with runtime
     }
 
     public static void sleep() {
